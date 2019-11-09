57e60d8616cd9e58fe6180be87c354d6ebeb4c35
Andrew Chen <andrewchen747@gmail.com>
2019-11-08 18:13:02 -0800

    nearly done part 1

--- db/migrate/20191109014636_create_pokemons.rb
+++ db/migrate/20191109014636_create_pokemons.rb
@@ -3,7 +3,7 @@ class CreatePokemons < ActiveRecord::Migration[6.0]
     create_table :pokemons do |t|
       t.string :name
       t.integer :level
-      t.references :trainer, null: false, foreign_key: true
+      t.references :trainer, foreign_key: true
       t.integer :ndex
 
       t.timestamps
